624b1a92e26654221492ddf1f5bb3c04ddab6ca8
MDIDKT <modidiakite@hotmail.fr>
2024-10-15 21:40:37 +0200

    test de mise en place de webpack

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,22 @@
+/*
 import { startStimulusApp } from '@symfony/stimulus-bundle';
 
 const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
+*/
+/*import { Controller } from '@hotwired/stimulus';
+
+/!*
+export default class extends Controller {
+    connect() {
+        this.element.textContent = 'Hello Stimulus! Edit me in assets/controllers/hello_controller.js';
+    }
+}*!/*/
+
+// assets/bootstrap.js
+
+import { startStimulusApp } from '@symfony/stimulus-bridge';
+
+// Start the Stimulus application
+startStimulusApp();
