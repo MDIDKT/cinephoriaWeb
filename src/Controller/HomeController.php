239a475e5a4fb5c90ceef89e4cc7952c0ff1438a
MDIDKT <modidiakite@hotmail.fr>
2024-10-23 12:57:57 +0200

    mise en page de la home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Films;
+use App\Repository\FilmsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,19 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(FilmsRepository $filmsRepository): Response
     {
+        $films = $filmsRepository->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'films' => $films,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_home_show', methods: ['GET'])]
+    public function show(Films $film): Response
+    {
+        return $this->render('films/show.html.twig', [
+            'film' => $film,
         ]);
     }
 }
