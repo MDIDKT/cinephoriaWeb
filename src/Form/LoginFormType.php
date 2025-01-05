74e4188bb3d06b569e007a305351db0a3f20a550
MDIDKT <152218331+MDIDKT@users.noreply.github.com>
2025-01-05 19:09:06 +0100

    r

--- src/Form/LoginFormType.php
+++ src/Form/LoginFormType.php
@@ -6,11 +6,20 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
 class LoginFormType extends AbstractType
 {
+    private CsrfTokenManagerInterface $csrfTokenManager;
+
+    public function __construct(CsrfTokenManagerInterface $csrfTokenManager)
+    {
+        $this->csrfTokenManager = $csrfTokenManager;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -21,6 +30,10 @@ class LoginFormType extends AbstractType
             ->add('password', PasswordType::class, [
                 'label' => 'Password',
                 'required' => true,
+            ])
+            ->add('csrf_token', HiddenType::class, [
+                'data' => $this->csrfTokenManager->getToken('authenticate')->getValue(),
+                'mapped' => false,
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Login',
