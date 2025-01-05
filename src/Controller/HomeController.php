750089b0f9837a3d43bf392f5129501dc49e56f7
MDIDKT <152218331+MDIDKT@users.noreply.github.com>
2025-01-05 17:58:26 +0100

    ss

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\Films;
 use App\Repository\FilmsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,6 +15,7 @@ class HomeController extends AbstractController
     {
         $films = $filmsRepository->findAll ();
         return $this->render ('home/index.html.twig', [
+            'films' => $films,
             'filmsLast' => $filmsRepository->find3LastFilms (),
         ]);
     }
