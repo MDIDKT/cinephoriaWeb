2f3dc55fbed2fa30aa2186c288a9f53de0a0b2fd
MDIDKT <152218331+MDIDKT@users.noreply.github.com>
2025-01-05 17:37:58 +0100

    d

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,18 +11,20 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-  /*  #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index (FilmsRepository $filmsRepository): Response
     {
         $films = $filmsRepository->findAll ();
         return $this->render ('home/index.html.twig', [
             'filmsLast' => $filmsRepository->find3LastFilms (),
         ]);
-    }*/
+    }
 
     #[Route('/films/{id}', name: 'app_home_show', methods: ['GET'])]
     public function show (Films $film): Response
     {
+        // Ajouter la colonne 'updated_at' dans la requête
+        $film->getUpdatedAt();
         return $this->render ('films/show.html.twig', [
             'film' => $film,
         ]);
