078e18d9d28cb882ebb13141f649dae733c5f86a
MDIDKT <152218331+MDIDKT@users.noreply.github.com>
2025-01-05 18:51:51 +0100

    hu

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\LoginFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -12,21 +14,24 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login (AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError ();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername ();
 
-        return $this->render ('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        $form = $this->createForm(LoginFormType::class, [
+            'email' => $lastUsername,
+        ]);
+
+        return $this->render ('security/login.html.twig', [
+            'form' => $form->createView(),
+            'error' => $error,
+        ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout (): void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
