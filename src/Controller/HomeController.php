83568e1680f1f8be47e850dd2e4c381699008cc0
MDIDKT <152218331+MDIDKT@users.noreply.github.com>
2025-01-06 22:18:31 +0100

    mise en place de la connectionn et register

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,15 +4,19 @@ namespace App\Controller;
 
 use App\Entity\Films;
 use App\Repository\FilmsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index (FilmsRepository $filmsRepository): Response
+    public function index (FilmsRepository $filmsRepository, EntityManagerInterface $em, UserPasswordHasherInterface $hasher): Response
     {
+
+
         $films = $filmsRepository->findAll ();
         return $this->render ('home/index.html.twig', [
             'films' => $films,
